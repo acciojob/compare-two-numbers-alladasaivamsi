0d3f88c8ae155b5b510aa494fe5c6db6b7e57d68
alladasaivamsi <109912485+alladasaivamsi@users.noreply.github.com>
2023-05-24 16:42:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
-const input1 = promt("Enter Input1:");
-const input2 = promt("Enter Input2:");
+const input1 = prompt("Enter Input1:");
+const input2 = prompt("Enter Input2:");
 if(input1 == input2)
 {
 	alert("true");
